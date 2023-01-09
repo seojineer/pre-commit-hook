0c079119bb683d61926e19a757a60abae3fca160
seoji kim <seoji.kim@fadutec.com>
2023-01-09 07:54:13 +0000

    fix(checkpatch): use filename to get diff

--- pre_commit_hooks/tools/git_diff.py
+++ pre_commit_hooks/tools/git_diff.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python3
 
+import sys
+
 from .cmd import run
 
 
-def get_diff(cached=False):
+def get_diff(cached=False, files=None):
     """Get git revision diff
 
     Args:
         cached(bool): if True get diff in stage area. else, get diff from HEAD
+        files(list): file list
 
     Returns:
         diff(str): git diff string
     """
+    if files is None:
+        sys.exit(0)
 
     if cached:
-        cmd = ["git", "diff", "--cached", "--", "'*.[chS]'"]
+        cmd = ["git", "diff", "--cached", "--", " ".join(files)]
     else:
-        cmd = ["git", "diff", "--", "'*.[chS]'"]
+        cmd = ["git", "diff", "--", " ".join(files)]
 
     return run(cmd, shell=True).stdout
